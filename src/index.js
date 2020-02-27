924229350e1ba84f4dfedd31aaacd8a5ecc3e542
DengZhiGuo <bobdeng@163.com>
2020-02-27 15:53:10 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { taxiFee } from './taxi';
 import { readFileLines } from './readfile';
 import { parseLine } from './parseline';
+
+function getTaxiFee(line) {
+  const taxi = parseLine(line);
+  return taxiFee(taxi.distance, taxi.waitingMinutes);
+}
+
 export async function main(testDataFile = 'testData.txt') {
   const lines = await readFileLines(`src/fixtures/${testDataFile}`);
-  const receipt = lines.map((line) => {
-    const taxi = parseLine(line);
-    return taxiFee(taxi.distance, taxi.waitingMinutes);
-
-  }).join('\n');
+  const receipt = lines.map(getTaxiFee).join('\n');
   console.log(receipt);
   return receipt;
 }
