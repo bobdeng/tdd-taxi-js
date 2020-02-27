956aea468b17ae87e266efe9d0a622f600c24446
DengZhiGuo <bobdeng@163.com>
2020-02-27 15:46:33 +0800

    refactor

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,3 +1,6 @@
-test('should ', () => {
-  expect(1 + 1).toBe(2);
+import { main } from '../index.js';
+
+test('parse default ', async () => {
+  expect(await main())
+    .toBe('6\n7\n13\n7');
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import { taxiFee } from './taxi';
 import { readFileLines } from './readfile';
-
-
-export default async function main(testDataFile = 'testData.txt') {
+import { parseLine } from './parseline';
+export async function main(testDataFile = 'testData.txt') {
   const lines = await readFileLines(`src/fixtures/${testDataFile}`);
   const receipt = lines.map((line) => {
     const taxi = parseLine(line);
     return taxiFee(taxi.distance, taxi.waitingMinutes);
 
-  })
-    .join('\n');
+  }).join('\n');
   console.log(receipt);
   return receipt;
 }
